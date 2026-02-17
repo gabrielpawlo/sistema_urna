202bd1227ca0555b26693c0520659ab061c5136d
Gabriel Pawlowski <gabrielgpawlowski@gmail.com>
2026-02-17 17:05:07 -0300

    oque fazer urna

--- Urna.java
+++ Urna.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+
 public class Urna {
+ /*
+ list cargos
+ int votosBrancos
+ int votosNulos
+ 
+ Urna()
+ addCargo()
+ iniciarVotacao()
+ mostrarResultado()
+ */
     
 }
