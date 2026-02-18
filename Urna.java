ecef563e59e2be597a3c643496f7ecd650fef96f
Gabriel Pawlowski <gabrielgpawlowski@gmail.com>
2026-02-18 17:06:01 -0300

    metodos de cadastro

--- Urna.java
+++ Urna.java
@@ -24,6 +24,7 @@ public class Urna {
                 System.out.println("2 - Cadastro de candidato");
                 System.out.println("3 - Iniciar votacao");
                 System.out.println("4 - SAIR");
+                System.out.println("");;
 
                 opcao = ler.nextInt();
                 ler.nextLine();
@@ -59,8 +60,47 @@ public class Urna {
     }
 
     private void cadastrarCargo(){
+        System.out.println("\nCADASTRO DE CARGO");
+        System.out.println("Nome do cargo: ");
+        String nome = ler.nextLine();
+
+        cargos.add(new Cargo(nome));
+        System.out.println("Cargo adicionado");
     }
-    private void cadastrarCandidato(){}
+
+    private void cadastrarCandidato(){
+        if (cargos.isEmpty()){
+            System.out.println("Nao e possivel, pois nao tem nenhum cargo");
+            return;
+        }
+
+        System.out.println("\nCADASTRO DE CANDIDATO");
+        System.out.println("Escolha o cargo:");
+
+        for (int i = 0; i < cargos.size(); i++) {
+            System.out.println((i + 1) + " - " + cargos.get(i).getNome());
+        }
+        
+        int escolha = ler.nextInt();
+        ler.nextLine();
+
+        Cargo cargo = cargos.get(escolha - 1);
+
+            System.out.print("Digite o nome do candidato: ");
+        String nome = ler.nextLine();
+
+        System.out.print("Digite o número (3 dígitos, diferente de 999): ");
+        int numero = ler.nextInt();
+
+        if (numero == 999 || numero > 999) {
+            System.out.println("Número inválido!");
+            return;
+        }
+        cargo.adicionarCandidato(new Candidato(nome, numero));
+
+        System.out.println("Candidato registrado!");
+    }
+
     private void iniciarVotacao(){}
     private void confirmarVoto(){}
     private void encerrarEleicao(){}
